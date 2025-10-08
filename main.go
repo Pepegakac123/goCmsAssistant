7dc57ab80249822c832eaaa3baade9c512b9ed33
Pepegakac123 <kacpperr.adamczyk@gmail.com>
2025-10-08 16:21:36 +0200

    Added base server setup

--- main.go
+++ main.go
@@ -1,1 +1,51 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type apiConfig struct {
+	Port       string
+	assetsRoot string
+}
+
+func main() {
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable is not set")
+	}
+	assetsRoot := os.Getenv("ASSETS_ROOT")
+	if assetsRoot == "" {
+		log.Fatal("ASSETS_ROOT environment variable is not set")
+	}
+
+	cfg := apiConfig{
+		Port:       port,
+		assetsRoot: assetsRoot,
+	}
+
+	err := cfg.ensureAssetsDir()
+	if err != nil {
+		log.Fatalf("Couldn't create assets directory: %v", err)
+	}
+
+	mux := http.NewServeMux()
+	// mux.Handle("/",)
+	assetsHandler := http.StripPrefix("/assets", http.FileServer(http.Dir(assetsRoot)))
+
+	mux.Handle("/assets/", assetsHandler)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving on: http://localhost:%s/app/\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
